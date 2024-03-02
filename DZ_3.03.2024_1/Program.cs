f6fdde958529aba0cb4acca9b4d1cfad692944d7
Андрей <dusha22075@gmail.com>
2024-03-02 11:38:53 +0300

    Добавьте файлы проекта.

---
+++ DZ_3.03.2024_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+
+// Напишите метод, который отображает квадрат из некоторого символа.
+// Метод принимает в качестве параметра: длину стороны квадрата, символ.
+
+class Program
+{
+    static void Main()
+    {
+        WriteLine("Введите длину стороны квадрата:");
+        int sideLength = Convert.ToInt32(ReadLine());
+
+        Write("Введите символ для отображения квадрата: ");
+        char symbol = ReadKey().KeyChar;
+
+        WriteLine("\n");
+        DrawSquare(sideLength, symbol);
+    }
+
+    static void DrawSquare(int sideLength, char symbol)
+    {
+        for (int i = 0; i < sideLength; i++)
+        {
+            for (int j = 0; j < sideLength; j++)
+            {
+                Write(symbol + " ");
+            }
+            WriteLine();
+        }
+    }
+}
